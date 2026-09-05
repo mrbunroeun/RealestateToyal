5397861f8ad9cef21ff3eb766ca7fea8a38b47f8
mrbunroeun <bunroeunhas@gmail.com>
2026-09-05 10:53:05 +0700

    finish fix the import

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup>
 import { ref, onMounted, onUnmounted } from "vue";
 import { RouterView } from "vue-router";
-// import Navbar from "./components/Navigation/Navbar.vue";
+import Navbar from "./components/navigation/Navbar.vue";
 
 const showScrollTop = ref(false);
 
@@ -69,4 +69,3 @@ onUnmounted(() => {
     </Transition>
   </div>
 </template>
-
